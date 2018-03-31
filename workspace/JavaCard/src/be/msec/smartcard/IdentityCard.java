aebbcae0e54d0cad316d17148d2c5cfa619f2aff
tommeke303 <Thomas@pcThomas.local>
2018-03-31 21:43:25 +0200

    foute push gedaan hiervoor, oooeps

---
+++ workspace/JavaCard/src/be/msec/smartcard/IdentityCard.java
@@ -0,0 +1,202 @@
+package be.msec.smartcard;
+
+
+
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+
+import javacard.framework.APDU;
+import javacard.framework.Applet;
+import javacard.framework.ISO7816;
+import javacard.framework.ISOException;
+import javacard.framework.OwnerPIN;
+import javacard.security.KeyBuilder;
+import javacard.security.RSAPrivateKey;
+import javacard.security.Signature;
+
+public class IdentityCard extends Applet {
+	private final static byte IDENTITY_CARD_CLA =(byte)0x80;
+	
+	private static final byte VALIDATE_PIN_INS = 0x22;
+	private static final byte GET_NAME_INS = 0x24;
+	private static final byte GET_SERIAL_INS = 0x26;
+	
+	private final static byte PIN_TRY_LIMIT =(byte)0x03;
+	private final static byte PIN_SIZE =(byte)0x04;
+	
+	private final static short SW_VERIFICATION_FAILED = 0x6300;
+	private final static short SW_PIN_VERIFICATION_REQUIRED = 0x6301;
+	
+	private final static short RSA_KEY = 0x30;
+
+	private byte[] privModulus = new byte[]{(byte)-73, (byte)-43, (byte)96, (byte)-107, (byte)82, (byte)25, (byte)-66, (byte)34, (byte)5, (byte)-58, (byte)75, (byte)-39, (byte)-54, (byte)43, (byte)25, (byte)-117, (byte)80, (byte)-62, (byte)51, (byte)19, (byte)59, (byte)-70, (byte)-100, (byte)85, (byte)24, (byte)-57, (byte)108, (byte)-98, (byte)-2, (byte)1, (byte)-80, (byte)-39, (byte)63, (byte)93, (byte)112, (byte)7, (byte)4, (byte)18, (byte)-11, (byte)-98, (byte)17, (byte)126, (byte)-54, (byte)27, (byte)-56, (byte)33, (byte)77, (byte)-111, (byte)-74, (byte)-78, (byte)88, (byte)70, (byte)-22, (byte)-3, (byte)15, (byte)16, (byte)37, (byte)-18, (byte)92, (byte)74, (byte)124, (byte)-107, (byte)-116, (byte)-125};
+	private byte[] privExponent = new byte[]{(byte)24, (byte)75, (byte)93, (byte)-79, (byte)62, (byte)33, (byte)98, (byte)-52, (byte)50, (byte)65, (byte)43, (byte)-125, (byte)3, (byte)-63, (byte)-64, (byte)101, (byte)117, (byte)-19, (byte)-60, (byte)60, (byte)53, (byte)119, (byte)-118, (byte)-13, (byte)-128, (byte)11, (byte)-46, (byte)-30, (byte)12, (byte)37, (byte)-125, (byte)14, (byte)104, (byte)-5, (byte)-15, (byte)-120, (byte)-113, (byte)-49, (byte)-70, (byte)-78, (byte)114, (byte)122, (byte)34, (byte)114, (byte)-99, (byte)-102, (byte)43, (byte)-43, (byte)-102, (byte)71, (byte)115, (byte)116, (byte)-105, (byte)-48, (byte)-80, (byte)109, (byte)117, (byte)106, (byte)88, (byte)6, (byte)-69, (byte)-42, (byte)-83, (byte)25};
+
+	private byte[] certificate = new byte[]{(byte)48, (byte)-126, (byte)1, (byte)-67, (byte)48, (byte)-126, (byte)1, (byte)103, (byte)-96, (byte)3, (byte)2, (byte)1, (byte)2, (byte)2, (byte)5, (byte)0, (byte)-73, (byte)-43, (byte)96, (byte)-107, (byte)48, (byte)13, (byte)6, (byte)9, (byte)42, (byte)-122, (byte)72, (byte)-122, (byte)-9, (byte)13, (byte)1, (byte)1, (byte)5, (byte)5, (byte)0, (byte)48, (byte)100, (byte)49, (byte)11, (byte)48, (byte)9, (byte)6, (byte)3, (byte)85, (byte)4, (byte)6, (byte)19, (byte)2, (byte)66, (byte)69, (byte)49, (byte)13, (byte)48, (byte)11, (byte)6, (byte)3, (byte)85, (byte)4, (byte)7, (byte)12, (byte)4, (byte)71, (byte)101, (byte)110, (byte)116, (byte)49, (byte)25, (byte)48, (byte)23, (byte)6, (byte)3, (byte)85, (byte)4, (byte)10, (byte)12, (byte)16, (byte)75, (byte)97, (byte)72, (byte)111, (byte)32, (byte)83, (byte)105, (byte)110, (byte)116, (byte)45, (byte)76, (byte)105, (byte)101, (byte)118, (byte)101, (byte)110, (byte)49, (byte)20, (byte)48, (byte)18, (byte)6, (byte)3, (byte)85, (byte)4, (byte)11, (byte)12, (byte)11, (byte)86, (byte)97, (byte)107, (byte)103, (byte)114, (byte)111, (byte)101, (byte)112, (byte)32, (byte)73, (byte)84, (byte)49, (byte)21, (byte)48, (byte)19, (byte)6, (byte)3, (byte)85, (byte)4, (byte)3, (byte)12, (byte)12, (byte)74, (byte)97, (byte)110, (byte)32, (byte)86, (byte)111, (byte)115, (byte)115, (byte)97, (byte)101, (byte)114, (byte)116, (byte)48, (byte)32, (byte)23, (byte)13, (byte)49, (byte)48, (byte)48, (byte)50, (byte)50, (byte)52, (byte)48, (byte)57, (byte)52, (byte)51, (byte)48, (byte)50, (byte)90, (byte)24, (byte)15, (byte)53, (byte)49, (byte)55, (byte)57, (byte)48, (byte)49, (byte)48, (byte)57, (byte)49, (byte)57, (byte)50, (byte)57, (byte)52, (byte)50, (byte)90, (byte)48, (byte)100, (byte)49, (byte)11, (byte)48, (byte)9, (byte)6, (byte)3, (byte)85, (byte)4, (byte)6, (byte)19, (byte)2, (byte)66, (byte)69, (byte)49, (byte)13, (byte)48, (byte)11, (byte)6, (byte)3, (byte)85, (byte)4, (byte)7, (byte)12, (byte)4, (byte)71, (byte)101, (byte)110, (byte)116, (byte)49, (byte)25, (byte)48, (byte)23, (byte)6, (byte)3, (byte)85, (byte)4, (byte)10, (byte)12, (byte)16, (byte)75, (byte)97, (byte)72, (byte)111, (byte)32, (byte)83, (byte)105, (byte)110, (byte)116, (byte)45, (byte)76, (byte)105, (byte)101, (byte)118, (byte)101, (byte)110, (byte)49, (byte)20, (byte)48, (byte)18, (byte)6, (byte)3, (byte)85, (byte)4, (byte)11, (byte)12, (byte)11, (byte)86, (byte)97, (byte)107, (byte)103, (byte)114, (byte)111, (byte)101, (byte)112, (byte)32, (byte)73, (byte)84, (byte)49, (byte)21, (byte)48, (byte)19, (byte)6, (byte)3, (byte)85, (byte)4, (byte)3, (byte)12, (byte)12, (byte)74, (byte)97, (byte)110, (byte)32, (byte)86, (byte)111, (byte)115, (byte)115, (byte)97, (byte)101, (byte)114, (byte)116, (byte)48, (byte)92, (byte)48, (byte)13, (byte)6, (byte)9, (byte)42, (byte)-122, (byte)72, (byte)-122, (byte)-9, (byte)13, (byte)1, (byte)1, (byte)1, (byte)5, (byte)0, (byte)3, (byte)75, (byte)0, (byte)48, (byte)72, (byte)2, (byte)65, (byte)0, (byte)-73, (byte)-43, (byte)96, (byte)-107, (byte)82, (byte)25, (byte)-66, (byte)34, (byte)5, (byte)-58, (byte)75, (byte)-39, (byte)-54, (byte)43, (byte)25, (byte)-117, (byte)80, (byte)-62, (byte)51, (byte)19, (byte)59, (byte)-70, (byte)-100, (byte)85, (byte)24, (byte)-57, (byte)108, (byte)-98, (byte)-2, (byte)1, (byte)-80, (byte)-39, (byte)63, (byte)93, (byte)112, (byte)7, (byte)4, (byte)18, (byte)-11, (byte)-98, (byte)17, (byte)126, (byte)-54, (byte)27, (byte)-56, (byte)33, (byte)77, (byte)-111, (byte)-74, (byte)-78, (byte)88, (byte)70, (byte)-22, (byte)-3, (byte)15, (byte)16, (byte)37, (byte)-18, (byte)92, (byte)74, (byte)124, (byte)-107, (byte)-116, (byte)-125, (byte)2, (byte)3, (byte)1, (byte)0, (byte)1, (byte)48, (byte)13, (byte)6, (byte)9, (byte)42, (byte)-122, (byte)72, (byte)-122, (byte)-9, (byte)13, (byte)1, (byte)1, (byte)5, (byte)5, (byte)0, (byte)3, (byte)65, (byte)0, (byte)33, (byte)97, (byte)121, (byte)-25, (byte)43, (byte)-47, (byte)113, (byte)-104, (byte)-11, (byte)-42, (byte)-46, (byte)-17, (byte)1, (byte)-38, (byte)50, (byte)59, (byte)-63, (byte)-74, (byte)-33, (byte)90, (byte)92, (byte)-59, (byte)99, (byte)-17, (byte)-60, (byte)17, (byte)25, (byte)79, (byte)68, (byte)68, (byte)-57, (byte)-8, (byte)-64, (byte)35, (byte)-19, (byte)-114, (byte)110, (byte)-116, (byte)31, (byte)-126, (byte)-24, (byte)54, (byte)71, (byte)82, (byte)-53, (byte)-78, (byte)-84, (byte)-45, (byte)-83, (byte)87, (byte)68, (byte)124, (byte)-1, (byte)-128, (byte)-49, (byte)124, (byte)103, (byte)28, (byte)56, (byte)-114, (byte)-10, (byte)97, (byte)-78, (byte)54};
+	
+	
+	private byte[] serial = new byte[]{(byte)0x4A, (byte)0x61, (byte)0x6e};
+	private byte[] name = new byte[]{0x4A, 0x61, 0x6E, 0x20, 0x56, 0x6F, 0x73, 0x73, 0x61, 0x65, 0x72, 0x74};
+	private OwnerPIN pin;
+	
+	private IdentityCard() {
+		/*
+		 * During instantiation of the applet, all objects are created.
+		 * In this example, this is the 'pin' object.
+		 */
+		pin = new OwnerPIN(PIN_TRY_LIMIT,PIN_SIZE);
+		pin.update(new byte[]{0x01,0x02,0x03,0x04},(short) 0, PIN_SIZE);
+		
+		/*
+		 * This method registers the applet with the JCRE on the card.
+		 */
+		register();
+	}
+
+	/*
+	 * This method is called by the JCRE when installing the applet on the card.
+	 */
+	public static void install(byte bArray[], short bOffset, byte bLength)
+			throws ISOException {
+		new IdentityCard();
+	}
+	
+	/*
+	 * If no tries are remaining, the applet refuses selection.
+	 * The card can, therefore, no longer be used for identification.
+	 */
+	public boolean select() {
+		if (pin.getTriesRemaining()==0)
+			return false;
+		return true;
+	}
+
+	/*
+	 * This method is called when the applet is selected and an APDU arrives.
+	 */
+	public void process(APDU apdu) throws ISOException {
+		//A reference to the buffer, where the APDU data is stored, is retrieved.
+		byte[] buffer = apdu.getBuffer();
+		
+		//If the APDU selects the applet, no further processing is required.
+		if(this.selectingApplet())
+			return;
+		
+		//Check whether the indicated class of instructions is compatible with this applet.
+		if (buffer[ISO7816.OFFSET_CLA] != IDENTITY_CARD_CLA)ISOException.throwIt(ISO7816.SW_CLA_NOT_SUPPORTED);
+		//A switch statement is used to select a method depending on the instruction
+		switch(buffer[ISO7816.OFFSET_INS]){
+		case VALIDATE_PIN_INS:
+			validatePIN(apdu);
+			break;
+		case GET_SERIAL_INS:
+			getSerial(apdu);
+			break;
+		case GET_NAME_INS:
+			getName(apdu);
+			break;
+		case RSA_KEY:
+			challenge(apdu);
+			break;
+		//If no matching instructions are found it is indicated in the status word of the response.
+		//This can be done by using this method. As an argument a short is given that indicates
+		//the type of warning. There are several predefined warnings in the 'ISO7816' class.
+		default: ISOException.throwIt(ISO7816.SW_INS_NOT_SUPPORTED);
+		}
+	}
+	
+	
+	private void challenge(APDU apdu) {
+		short offset = 0;
+		short keySizeInBytes = 64;
+		short keySizeInBits = 512;
+		RSAPrivateKey privKey = ( RSAPrivateKey ) KeyBuilder . buildKey (KeyBuilder .TYPE_RSA_PRIVATE, keySizeInBits , false ); 
+		privKey.setExponent(privExponent, offset , keySizeInBytes); 
+		privKey.setModulus(privModulus, offset , keySizeInBytes);
+		
+		Signature signature = Signature.getInstance(Signature.ALG_RSA_SHA_PKCS1, false);
+		signature.init (privKey , Signature.MODE_SIGN);
+		
+		
+		
+		try {
+			SecureRandom random;
+			random = SecureRandom.getInstance ("SHA1PRNG");
+			byte[] bytes = new byte[20];
+			random.nextBytes(bytes);
+			byte[] output = new byte[100];
+			short sigLength = signature.sign(bytes, offset , (short) 20, output, (short) 0);
+		} catch (NoSuchAlgorithmException e) {
+			// TODO Auto-generated catch block
+			System.out.println(e);
+		}
+
+		
+		
+	}
+	/*
+	 * This method is used to authenticate the owner of the card using a PIN code.
+	 */
+	private void validatePIN(APDU apdu){
+		byte[] buffer = apdu.getBuffer();
+		//The input data needs to be of length 'PIN_SIZE'.
+		//Note that the byte values in the Lc and Le fields represent values between
+		//0 and 255. Therefore, if a short representation is required, the following
+		//code needs to be used: short Lc = (short) (buffer[ISO7816.OFFSET_LC] & 0x00FF);
+		if(buffer[ISO7816.OFFSET_LC]==PIN_SIZE){
+			//This method is used to copy the incoming data in the APDU buffer.
+			apdu.setIncomingAndReceive();
+			//Note that the incoming APDU data size may be bigger than the APDU buffer 
+			//size and may, therefore, need to be read in portions by the applet. 
+			//Most recent smart cards, however, have buffers that can contain the maximum
+			//data size. This can be found in the smart card specifications.
+			//If the buffer is not large enough, the following method can be used:
+			//
+			//byte[] buffer = apdu.getBuffer();
+			//short bytesLeft = (short) (buffer[ISO7816.OFFSET_LC] & 0x00FF);
+			//Util.arrayCopy(buffer, START, storage, START, (short)5);
+			//short readCount = apdu.setIncomingAndReceive();
+			//short i = ISO7816.OFFSET_CDATA;
+			//while ( bytesLeft > 0){
+			//	Util.arrayCopy(buffer, ISO7816.OFFSET_CDATA, storage, i, readCount);
+			//	bytesLeft -= readCount;
+			//	i+=readCount;
+			//	readCount = apdu.receiveBytes(ISO7816.OFFSET_CDATA);
+			//}
+			if (pin.check(buffer, ISO7816.OFFSET_CDATA,PIN_SIZE)==false)
+				ISOException.throwIt(SW_VERIFICATION_FAILED);
+		}else ISOException.throwIt(ISO7816.SW_WRONG_LENGTH);
+	}
+	
+	/*
+	 * This method checks whether the user is authenticated and sends
+	 * the serial number.
+	 */
+	private void getSerial(APDU apdu){
+		//If the pin is not validated, a response APDU with the
+		//'SW_PIN_VERIFICATION_REQUIRED' status word is transmitted.
+		if(!pin.isValidated())ISOException.throwIt(SW_PIN_VERIFICATION_REQUIRED);
+		else{
+			//This sequence of three methods sends the data contained in
+			//'serial' with offset '0' and length 'serial.length'
+			//to the host application.
+			apdu.setOutgoing();
+			apdu.setOutgoingLength((short)serial.length);
+			apdu.sendBytesLong(serial,(short)0,(short)serial.length);
+		}
+	}
+	
+	private void getName(APDU apdu){
+		//If the pin is not validated, a response APDU with the
+		//'SW_PIN_VERIFICATION_REQUIRED' status word is transmitted.
+		if(!pin.isValidated())ISOException.throwIt(SW_PIN_VERIFICATION_REQUIRED);
+		else{
+			//This sequence of three methods sends the data contained in
+			//'serial' with offset '0' and length 'serial.length'
+			//to the host application.
+			apdu.setOutgoing();
+			apdu.setOutgoingLength((short)name.length);
+			apdu.sendBytesLong(name,(short)0,(short)name.length);
+		}
+	}
+}
