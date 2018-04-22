98a64a389e94107b3440b6e1e19fc8dd7ed0c85e
Pedrosso <joachim.alvarezrodriguez@gmail.com>
2018-04-22 21:36:05 +0200

    government timestamp service added

--- workspace/JavaCard/converter.sh
+++ workspace/JavaCard/converter.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export JC_HOME="/home/steckdenis/Java/java_card_kit-2_2_2"
+export JC_HOME="/media/sf_Security-Project/java_card_kit-2_2_2"
 export CLASSES="$JC_HOME/lib/apduio.jar:$JC_HOME/lib/apdutool.jar:$JC_HOME/lib/jcwde.jar:$JC_HOME/lib/converter.jar:$JC_HOME/lib/scriptgen.jar:$JC_HOME/lib/offcardverifier.jar:$JC_HOME/lib/api.jar:$JC_HOME/lib/installer.jar:$JC_HOME/lib/capdump.jar:$JC_HOME/samples/classes:$CLASSPATH"
 
 echo $CLASSES
