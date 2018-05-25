8ac5f322dfc9e72c19120df0038fc189099fa19d
Pedrosso <joachim.alvarezrodriguez@gmail.com>
2018-05-25 23:11:54 +0200

    step 2, (1)->(7) code gemaakt (staat nu voorlopig in client, moet uiteindelijk in javacard gebeuren)

--- workspace/Middleware/src/be/msec/client/Client.java
+++ workspace/Middleware/src/be/msec/client/Client.java
@@ -4,15 +4,33 @@ import be.msec.client.connection.Connection;
 import be.msec.client.connection.IConnection;
 import be.msec.client.connection.SimulatedConnection;
 import be.msec.government.client.GVMTimestampClient;
+import be.msec.serviceProvider.SPmessage;
 import be.msec.serviceProvider.client.SPClient;
+import javafx.util.Pair;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.ObjectInput;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutput;
+import java.io.ObjectOutputStream;
 import java.nio.file.Paths;
+import java.security.Key;
+import java.security.PublicKey;
 import java.security.SecureRandom;
 import java.security.cert.X509Certificate;
 import java.sql.Timestamp;
+import java.util.Random;
 
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
 import javax.smartcardio.*;
+import javax.sql.rowset.serial.SerialBlob;
+
+import com.sun.org.apache.xml.internal.serializer.SerializerBase;
+import com.sun.xml.internal.bind.v2.runtime.unmarshaller.XsiNilLoader.Array;
+import com.sun.xml.internal.ws.util.Pool.TubePool;
 
 public class Client {
 
@@ -123,17 +141,52 @@ public class Client {
 			 */
 			SPClient service = new SPClient();
 
+			// TODO (1) send this certificate to the javacard 
 			X509Certificate cert = service.getServiceCertificate();
-			// TODO send this certificate to the javacard
 
 			/**
 			 * TODO stuff to do on javacard
-			 * 
-			 * // verify certificate
-			 * cert.verify(*CA pk*); // (2)
-			 * cert.checkValidity(*lastValidationTime*); // (3)
+			 * (2)-(8)
 			 */
 
+			 // verify certificate
+			// cert.verify(*CA pk*); // (2)
+			cert.checkValidity(timestamp); // (3), use lastValidationTime instead of timestamp
+			 
+			// (4) make symmetric key
+			String algSym = "AES";
+			Cipher cph = Cipher.getInstance(algSym);
+			Key symKey = KeyGenerator.getInstance("AES").generateKey();
+
+			// (5) encrypt symKey with pk of cert
+			String algAsym = "RSA/ECB/PKCS1Padding";
+			PublicKey pk = cert.getPublicKey();
+			cph = Cipher.getInstance(algAsym);
+			System.out.println("alg: " + pk.getAlgorithm());
+			cph.init(Cipher.ENCRYPT_MODE, pk);
+			byte[] encryptedSymKey = cph.doFinal(symKey.getEncoded());
+
+			// (6) generate random nr
+			Random rand = new Random();
+			int n = rand.nextInt();
+			System.out.println("Random: " + n);
+
+			// (7)
+			Pair<Integer, X509Certificate> msg = new Pair<Integer, X509Certificate>(n, cert);
+			
+			cph = Cipher.getInstance(algSym);
+			cph.init(Cipher.ENCRYPT_MODE, symKey);
+			byte[] encryptedMsg = cph.doFinal(convertToBytes(msg));
+
+			/**
+			// conversion tests (working)
+			Pair<Integer, X509Certificate> test = (Pair<Integer, X509Certificate>) convertToObject(convertToBytes(msg));
+			System.out.println("Original: " + msg.toString());
+			System.out.println("Converted twice: " + test.toString());
+			*/
+
+			// TODO (8) send encryptedMsg & encryptedSymKey to middelware
+			
 			// just to keep client running
 			while (true) {
 
@@ -143,6 +196,69 @@ public class Client {
 			throw e;
 		} finally {
 			c.close(); // close the connection with the card
+		}
+	}
+
+	// search for an attribute
+	public static String getCertificateServiceName(X509Certificate cert) {
+		return getCertificateAttribute(cert, "CN");
+	}
+
+	public static String getCertificateDomain(X509Certificate cert) {
+		return getCertificateAttribute(cert, "DC");
+	}
+
+	public static String getCertificateAttribute(X509Certificate cert, String Attribute) {
+		String input = cert.getSubjectDN().toString();
+		String output = "";
+
+		// loop through all attributes
+		for (String attr : input.split(", ")) {
+			String[] details = attr.split("=");
+			String attrName = details[0];
+			String attrValue = details[1];
+
+			// if correct attribute, set output & stop
+			if (attrName.toLowerCase().equals(Attribute.toLowerCase())) {
+				output = attrValue;
+				break;
+			}
+		}
+
+		return output;
+	}
+
+	public static byte[] convertToBytes(Object input) {
+		ByteArrayOutputStream bos = new ByteArrayOutputStream();
+		ObjectOutput out = null;
+		try {
+			out = new ObjectOutputStream(bos);
+			out.writeObject(input);
+			out.flush();
+			byte[] yourBytes = bos.toByteArray();
+
+			bos.close();
+
+			return yourBytes;
+		} catch (Exception ex) {
+			return null;
+		}
+	}
+
+	public static Object convertToObject(byte[] yourBytes) {
+		ByteArrayInputStream bis = new ByteArrayInputStream(yourBytes);
+		ObjectInput in = null;
+		try {
+			in = new ObjectInputStream(bis);
+			Object o = in.readObject();
+
+			in.close();
+
+			return o;
+		} catch (Exception ex) {
+			// ignore close exception
+
+			return null;
 		}
 	}
 }
